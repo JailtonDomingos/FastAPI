2c8ba1b0021ab0ce73c55e00b839b9dae4533777
Jailton Domingos <jailton.domingos@sumicity.net.br>
2023-12-11 20:38:44 -0300

    Inicializing

---
+++ main.py
@@ -0,0 +1,14 @@
+## FastAPI - Construção de API
+## Uvicorn - Hospedagem da API local
+
+# Fast
+# Documentação automática
+# Gerenciamento de processos assíncronos
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
